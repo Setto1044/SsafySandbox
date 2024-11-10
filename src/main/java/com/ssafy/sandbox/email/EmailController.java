9ba4f4e7006ed5b5ffc391b047245a239e508783
Setto1044 <baesj1044@gmail.com>
2024-11-10 22:25:08 +0900

    feat: create branch week 3, Initialized Email Controller

---
+++ src/main/java/com/ssafy/sandbox/email/EmailController.java
@@ -0,0 +1,10 @@
+package com.ssafy.sandbox.email;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+@RequestMapping("/email")
+public class EmailController {
+}
